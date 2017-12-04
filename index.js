afdaf626ffc861031830b79b617e0b9a37323035
Max <maxxporoshin@gmail.com>
2017-12-04 13:05:34 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+const port = 3000;
+
+
+router.get('/', async (ctx, next) => {
+  ctx.body = {
+    status: 200,
+    body: `I'm fine`,
+    error: null
+  };
+  console.log(`here I am at ${new Date()}`);
+});
+
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
